27c22458e4359e51e943797c5bc220317c85f4ea
AzamatCoder <60395192+qrbazamat@users.noreply.github.com>
2021-11-12 17:53:49 +0500

    Add Apps Section

---
+++ src/Components/AppsMenu/MyAppsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import './CSS/MyAppsStyle.css'
+import MyReactSlick from './MyReactSlick'
+
+function MyAppsContainer() {
+    return (
+        <div className='my-apps-container d-flex align-items-center'>
+            <Container className='animate-apps'>
+                <MyReactSlick />
+            </Container>
+        </div>
+    )
+}
+
+export default MyAppsContainer
